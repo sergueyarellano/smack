6a8a51a3b8665cee5976bdb6acee99f751acc881
Dan Popovic <dan-popovic@hotmail.com>
2023-07-21 11:44:15 -0400

    feat (windows): draggable

--- react-app/src/components/terminal/index.js
+++ react-app/src/components/terminal/index.js
@@ -1,16 +1,19 @@
 import Output from './output'
 import Typewriter from './tty'
 import styles from './index.module.css'
+import Draggable from 'react-draggable'
 
 // TODO: cool feature would be to remember history and reselect messages and commands
 export default function Terminal ({ isVisible, isConnected, ttyStdout, dispatchCommand, sendMessage, logTty }) {
   return (
-    <div className={`${isVisible ? styles.main : styles.notVisible}`}>
-      <label className={`${isConnected ? styles.isConnected : styles.isNotConnected} ${styles.statusConnected}`}>
-        ws
-      </label>
-      <Output stdout={ttyStdout} />
-      <Typewriter dispatchCommand={dispatchCommand} dispatchMessage={sendMessage} logTty={logTty} focus={isVisible} />
-    </div>
+    <Draggable>
+      <div className={`${isVisible ? styles.main : styles.notVisible}`}>
+        <label className={`${isConnected ? styles.isConnected : styles.isNotConnected} ${styles.statusConnected}`}>
+          ws
+        </label>
+        <Output stdout={ttyStdout} />
+        <Typewriter dispatchCommand={dispatchCommand} dispatchMessage={sendMessage} logTty={logTty} focus={isVisible} />
+      </div>
+    </Draggable>
   )
 }
--- react-app/src/programs/rtc/view.js
+++ react-app/src/programs/rtc/view.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react'
 import style from './view.module.css'
+import Draggable from 'react-draggable'
 
 export default function VideoConfView ({ localStream, remoteStream }) {
   const refLocal = useRef(null)
@@ -16,8 +17,12 @@ export default function VideoConfView ({ localStream, remoteStream }) {
   }, [remoteStream])
   return (
     <div className={style.main}>
-      <video className={style.vid} id='webcamVideo' autoPlay playsInline muted ref={refLocal} />
-      <video className={style.vid} id='remoteVideo' autoPlay playsInline ref={refRemote} />
+      <Draggable>
+        <video className={style.vid} id='webcamVideo' autoPlay playsInline muted ref={refLocal} />
+      </Draggable>
+      <Draggable>
+        <video className={style.vid} id='remoteVideo' autoPlay playsInline ref={refRemote} />
+      </Draggable>
     </div>
   )
 }
