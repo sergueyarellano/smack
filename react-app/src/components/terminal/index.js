3b3088f864d8847aec02c6bf29a5ecd30da89865
sergueyarellano <sarellano85@gmail.com>
2023-07-27 10:15:01 -0400

    fix: remove draggable
    * it interfered when selecting text from output

--- react-app/src/components/terminal/index.js
+++ react-app/src/components/terminal/index.js
@@ -1,22 +1,17 @@
 import Output from './output'
 import Typewriter from './tty'
 import styles from './index.module.css'
-import Draggable from 'react-draggable'
-import { useState } from 'react'
 
 // TODO: cool feature would be to remember history and reselect messages and commands
 export default function Terminal ({ isVisible, isConnected, ttyStdout, dispatchCommand, sendMessage, logTty, cleared }) {
-  const [clicked, setClicked] = useState(0)
-
   return (
-    <Draggable>
-      <div onClick={() => setClicked((prev) => ++prev)} className={`${isVisible ? styles.main : styles.notVisible}`}>
-        <label className={`${isConnected ? styles.isConnected : styles.isNotConnected} ${styles.statusConnected}`}>
-          ws
-        </label>
-        <Output stdout={ttyStdout} cleared={cleared} />
-        <Typewriter dispatchCommand={dispatchCommand} dispatchMessage={sendMessage} logTty={logTty} isVisible={isVisible} clicked={clicked} />
-      </div>
-    </Draggable>
+    <div className={`${isVisible ? styles.main : styles.notVisible}`}>
+      <label className={`${isConnected ? styles.isConnected : styles.isNotConnected} ${styles.statusConnected}`}>
+        ws
+      </label>
+      <Output stdout={ttyStdout} cleared={cleared} />
+      <Typewriter dispatchCommand={dispatchCommand} dispatchMessage={sendMessage} logTty={logTty} isVisible={isVisible} />
+    </div>
+
   )
 }
